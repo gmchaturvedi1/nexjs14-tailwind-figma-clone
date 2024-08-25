4f2aa17f101ff89c1919ca983f176e93f81d3490
Gajendra Chaturvedi <gchaturvedi@clarapth.com>
2024-08-25 18:24:51 +0530

    updates

---
+++ README.md
@@ -0,0 +1,9 @@
+  <div align="center">
+  Real Time Figma Clone with following technologies :  
+  </div>
+  <ul>
+    <li>Nextjs 14 </li>
+    <li>Radix Ui </li>
+    <li>Fabric</li>
+    <li>Tailwind</li>
+  </ul>
---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import { Work_Sans } from "next/font/google";
+
+import { TooltipProvider } from "@/components/ui/tooltip";
+import "./globals.css";
+
+import Room from "./Room";
+
+export const metadata = {
+  title: "Figma Clone",
+  description:
+    "A minimalist Figma clone using fabric.js and Liveblocks for realtime collaboration",
+};
+
+const workSans = Work_Sans({
+  subsets: ["latin"],
+  variable: "--font-work-sans",
+  weight: ["400", "600", "700"],
+});
+
+const RootLayout = ({ children }: { children: React.ReactNode }) => (
+  <html lang='en'>
+    <body className={`${workSans.className} `}>
+      <Room>
+        <TooltipProvider>{children}</TooltipProvider>
+      </Room>
+    </body>
+  </html>
+);
+
+export default RootLayout;
